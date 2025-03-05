92186679abd369a7bf4b2bddd3804327f52102f5
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-05 11:37:06 +0330

    Implement an error message when the request fails

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,56 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+const baseURL = "https://jsonplaceholder.typicode.com/posts/176";
 
 function App() {
   const [post, setPost] = useState<{ title: string; body: string } | null>(
     null
   );
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-      setTimeout(() => {
+    axios
+      .get(baseURL)
+      .then((response) => {
+        setPost(response.data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 3000);
+      })
+      .catch((err) => {
+        setError(err.message);
         setLoading(false);
-      }, 3000);
-    });
+      });
   }, []);
 
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => {
+        setError("");
+      }, 4000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
+
   if (loading) return <p>Loading...</p>;
-  if (!post) return null;
 
   return (
     <div>
-      <h1 className="text-lg font-bold">{post.title}</h1>
-      <p>{post.body}</p>
+      {error && (
+        <p className="max-w-2xl p-5 text-rose-500 bg-rose-100 rounded-md fixed bottom-2 right-2">
+          {error}
+          <span className="absolute bottom-0 right-0 h-1 w-0 bg-rose-500 animate-progress"></span>
+        </p>
+      )}
+      {post && (
+        <div>
+          <h1 className="text-lg font-bold">{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      )}
     </div>
   );
 }
