bb694f6c2cc4ca70250ba709f414e59e8fdb18a9
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-05 11:19:04 +0330

    Create loading mode when loading data

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,16 @@ function App() {
   const [post, setPost] = useState<{ title: string; body: string } | null>(
     null
   );
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(baseURL).then((response) => setPost(response.data));
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+      setLoading(false);
+    });
   }, []);
 
+  if (loading) return <p>Loading...</p>;
   if (!post) return null;
 
   return (
