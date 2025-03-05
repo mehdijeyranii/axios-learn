1dd22b512a8880fbdb0be12ae907d685561100b3
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-05 11:54:12 +0330

    Another way to receive data using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,37 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts/176";
+const baseURL = "http://localhost:5000/api";
 
-function App() {
-  const [post, setPost] = useState<{ title: string; body: string } | null>(
-    null
-  );
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+const App = () => {
+  const [posts, setPosts] = useState<
+    | {
+        id: number;
+        title: string;
+        body: string;
+      }[]
+    | null
+  >(null);
 
   useEffect(() => {
-    axios
-      .get(baseURL)
-      .then((response) => {
-        setPost(response.data);
-        setTimeout(() => {
-          setLoading(false);
-        }, 3000);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-      });
+    axios.get(baseURL).then((res) => {
+      setPosts(res.data.posts);
+    });
   }, []);
-
-  useEffect(() => {
-    if (error) {
-      const timer = setTimeout(() => {
-        setError("");
-      }, 4000);
-
-      return () => clearTimeout(timer);
-    }
-  }, [error]);
-
-  if (loading) return <p>Loading...</p>;
 
   return (
     <div>
-      {error && (
-        <p className="max-w-2xl p-5 text-rose-500 bg-rose-100 rounded-md fixed bottom-2 right-2">
-          {error}
-          <span className="absolute bottom-0 right-0 h-1 w-0 bg-rose-500 animate-progress"></span>
-        </p>
-      )}
-      {post && (
-        <div>
-          <h1 className="text-lg font-bold">{post.title}</h1>
-          <p>{post.body}</p>
-        </div>
-      )}
+      <h1 className="text-2xl font-bold">Posts</h1>
+      <ul>
+        {posts?.map((post) => (
+          <li key={post.id} className="border-b p-2">
+            <h2 className="font-semibold">{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
